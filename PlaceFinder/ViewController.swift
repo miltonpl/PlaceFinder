14f71dded75467012d6d9f8dd369663fb8cb574d
Milton Palaguachi <miltonpalaguachi@gmail.com>
2020-10-18 11:23:51 -0400

    Initial Commit

---
+++ PlaceFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaceFinder
+//
+//  Created by Milton Palaguachi on 10/18/20.
+//  Copyright © 2020 Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
